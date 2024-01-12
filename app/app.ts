5bf6235a9149b2c0acf4b07e82651b20e1a3b38d
jy <magician.of.technique@aliyun.com>
2024-01-12 13:57:14 +0800

    [*] init

---
+++ app/app.ts
@@ -0,0 +1,12 @@
+// app.ts
+App<IAppOption>({
+  globalData: {},
+  onLaunch() {
+    // wx.login({
+    //   success: res => {
+    //     console.log('login code',res.code)
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //   },
+    // })
+  },
+})
